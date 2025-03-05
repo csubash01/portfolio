e0f0d6fbc4c55fb2a05e59b4bd89f11f683510c7
csubash01 <tharusubas1000@gmail.com>
2025-03-05 22:12:25 +0545

    experience and contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form')
+  contactMessage = document.getElementById('contact-message')
+contst sendEmail = (e) =>{
+    e.preventDefault()
 
+    //serviceID - templateID - #form - publickey
+    emailjs.sendform('','','','')
+}
+contactForm.addEventListener('submit', sendEmail)
+  
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
